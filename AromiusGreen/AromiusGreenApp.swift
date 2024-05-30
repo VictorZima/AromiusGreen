c8cb03a1541a8cbeaf7674f67fb2c48608c35b55
VictorZima <victorzima@icloud.com>
2024-05-30 03:58:21 +0300

    Initial Commit

---
+++ AromiusGreen/AromiusGreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AromiusGreenApp.swift
+//  AromiusGreen
+//
+//  Created by VictorZima on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AromiusGreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

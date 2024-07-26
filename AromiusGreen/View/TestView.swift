c4ab8ea622c3a5181faac4faaab87fc152bf5080
VictorZima <victorzima@icloud.com>
2024-07-26 04:24:43 +0300

    Bug Fix

---
+++ AromiusGreen/View/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  AromiusGreen
+//
+//  Created by VictorZima on 07/07/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
